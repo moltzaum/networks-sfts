306ff77df3fc3858ad4bb22b62c22bbdfd9561fe
moltzaum <matthew@moltzau.net>
2019-04-10 22:53:05 -0600

    Major Changes to Server
    Accepting Multiple Clients
        TODO: Implement client limit
        TODO: Implement chat loop in server
        TODO: Implement chat loop in client
        TODO: Make Tests
        TODO: Make port passable as command line argument?
            (btw all programs should do --version and --help, as well as -h and -v)
    
    Adjusted some spacing

--- src/client.c
+++ src/client.c
@@ -1,6 +1,3 @@
-// Client side C/C++ program to demonstrate Socket programming 
-
-// Note: A good reference would be 'Unix Network Programming Volume 1'
 
 #include <stdio.h>  // perror, print, fgets
 #include <stdlib.h> // exit, atoi
@@ -8,7 +5,6 @@
 #include <string.h> // ?
 #include <errno.h>
 
-//#include <sys/types.h>  // pthreads
 #include <sys/socket.h> 
 #include <netinet/in.h> 
 #include <arpa/inet.h>
@@ -23,12 +19,12 @@ void error(char *msg) {
 
 int main(int argc, char const *argv[]) { 
     
-	  struct sockaddr_in address;
-	  struct sockaddr_in serv_addr;
-	  int sock = 0, valread;
-	  char *hello = "Hello from client";
-	  char buffer[1024] = {0};
-
+      struct sockaddr_in address;
+      struct sockaddr_in serv_addr;
+      int sock = 0, valread;
+      char *hello = "Hello from client";
+      char buffer[1024] = {0};
+    
     /*
     int portno;
     struct hostent *server;
@@ -50,44 +46,44 @@ int main(int argc, char const *argv[]) {
     }*/
     // server->h_name is server hostname and can be used in inet_pton
     // server->h_addr is server address and can be set to serv_addr.sin_addr.s_addr
-
+    
     // Initialize the socket
-	  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
-	  	printf("Socket creation error\n");
-	  	return -1;
-	  }
-
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+        printf("Socket creation error\n");
+        return -1;
+    }
+    
     // START SERV_ADDR STUFF (also related to above)
-
+    
     // Initialize Server Information
-	  memset(&serv_addr, '0', sizeof(serv_addr));
-	  serv_addr.sin_family = AF_INET;
-	  serv_addr.sin_port = htons(PORT);
-
-	  // Convert IPv4 and IPv6 addresses from text to binary form 
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    
+    // Convert IPv4 and IPv6 addresses from text to binary form 
     //
     // Note: inet_pton supports ipv4 and ipv6, but doesn't support ipv4
     // shorthand like inet_aton, which doesn't support ipv6.
-
-	  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) { 
-	  	printf("Invalid address/ Address not supported\n");
-	  	return -1;
-	  }
+    
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) { 
+        printf("Invalid address/ Address not supported\n");
+        return -1;
+    }
     
     // END SERV_ADDR STUFF
-
+    
     // Connect to sock
-	  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
-	  	printf("Connection Failed \n");
-	  	return -1;
-	  }
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
+        printf("Connection Failed \n");
+        return -1;
+    }
     
     // TODO: Create an input loop
-
-	  write(sock, hello, strlen(hello));
-	  printf("Hello message sent\n");
-	  valread = read(sock , buffer, 1024);
-
+    
+    write(sock, hello, strlen(hello));
+    printf("Hello message sent\n");
+    valread = read(sock , buffer, 1024);
+    
     // I can get the value returned from eax for some cases
     // This is not the case for odd return sizes
     // It does not work on Windows (I think)
@@ -100,11 +96,11 @@ int main(int argc, char const *argv[]) {
     // SetErrorTestType(INT_SENTINEL)
     // SetErrorTestType(ERRNO)
     //    might be able to set to 0 after handling
-
+    
     // valread = bytes read, errno is set to != 0
     // for safety, check valread to know if errno was updated instead of
     // trying to say errno = 0 before each function that _may_ update the errno
     // I can simply use perror, or use strerror(errno) in a formatted string
-	  printf("%s\n", buffer);
-	  return 0;
+    printf("%s\n", buffer);
+    return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,65 +1,96 @@
-// Server side C/C++ program to demonstrate Socket programming 
 
-#include <unistd.h> 
 #include <stdio.h> 
-#include <sys/socket.h> 
 #include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h>  // strlen
+
+#include <pthread.h>
+#include <sys/socket.h> 
 #include <netinet/in.h> 
-#include <string.h> 
 
 #define PORT 1042
 
-int main(int argc, char const *argv[])
-{ 
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
+struct client_args {
+    int sock;
+};
+
+void* client_handler(void *vargp) {
+    
+    // TODO: Loop input
+    // Call other functions for options
+    
+    struct client_args* args = (struct client_args*) vargp;
+    int sock = args->sock;
+    int valread;
+    
     char buffer[1024] = {0};
     char *hello = "Hello from server";
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    { 
+    
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+    send(sock, hello, strlen(hello), 0 );
+    printf("Hello message sent\n");
+    
+    return NULL;
+}
+
+int main(int argc, char const *argv[]) { 
+    
+    // TODO: Make port be a command line argument
+    
+    int server_fd;
+    struct sockaddr_in address;
+    int len = sizeof(address);
+    
+    // Shorthand for passing arguments
+    #define THIS server_fd, (struct sockaddr*) &address
+    
+    // Create server sock
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) { 
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
     
-    // This returns an error on mac, but not linux. It is non-fatal,
-    // and if I comment it out nothing seems to happen.
-    // 
-    // Forcefully attaching socket to the port 8080 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt)))
-    { 
-        //perror("setsockopt");
-        //exit(EXIT_FAILURE);
-    }
+    // Initialize server address information
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-       
-    // Forcefully attaching socket to the port 8080 
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0)
-    { 
+    address.sin_port = htons(PORT);
+    
+    if (bind(THIS, len) < 0) { 
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0)
-    { 
+    
+    if (listen(server_fd, 32) < 0) { 
         perror("listen");
         exit(EXIT_FAILURE);
     }
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0)
-    { 
+    
+    // Here I create a bunch of clients. The clients should
+    // be able to terminate themselves, but..
+    // 1) What happens if the client connection breaks?
+    // 2) Can I prevent this server from accepting too many clients?
+    // 3) #clients == #threads?
+    int sock;
+    pthread_t tid;
+    struct client_args args;
+    handle_next_client:
+    
+    if ((sock = accept(THIS, (socklen_t*) &len)) < 0) { 
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    valread = read( new_socket , buffer, 1024);
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+    
+    args.sock = sock;
+    pthread_create(&tid, NULL, client_handler, &args);
+    goto handle_next_client;
+    
     return 0;
 }
+
+// This waits for the thread (I think). I don't
+// this this is needed since I'm not computing
+// anything.
+//void* status
+//pthread_join(tid, &status);
+
