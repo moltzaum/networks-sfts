5a5328638c86b2e87bb183bc4ef0ebb06a8c8daf
moltzaum <matthew@moltzau.net>
2019-04-10 20:32:07 -0600

    Moved geeksforgeeks example out of examples

--- src/client.c
+++ src/client.c
@@ -1,75 +1,110 @@
-/* A simple client */
+// Client side C/C++ program to demonstrate Socket programming 
+
+// Note: A good reference would be 'Unix Network Programming Volume 1'
 
 #include <stdio.h>  // perror, print, fgets
 #include <stdlib.h> // exit, atoi
+#include <unistd.h> // read, write
+#include <string.h> // ?
 #include <errno.h>
 
-#include <sys/types.h> // ? for pthreads later I guess
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
+//#include <sys/types.h>  // pthreads
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <arpa/inet.h>
 
-#include <string.h> // bzero, bcopy (btw memset is preferred)
-#include <unistd.h> // read, write
+#define PORT 1042
 
-void error(char *msg)
-{
+// Potential helper method?
+void error(char *msg) {
     perror(msg);
-    exit(0);
+    exit(1);
 }
 
-int main(int argc, char *argv[])
-{
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-    char buffer[256];
+int main(int argc, char const *argv[]) { 
+    
+	  struct sockaddr_in address;
+	  struct sockaddr_in serv_addr;
+	  int sock = 0, valread;
+	  char *hello = "Hello from client";
+	  char buffer[1024] = {0};
 
+    /*
+    int portno;
+    struct hostent *server;
     // Usage Statement
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
-
+    
     // Fetch Port and Hostname from Args
+    // argv[1] can be ip
+    // argv[1] can be hostname
+    // can hostname be something from /etc/hosts?
     portno = atoi(argv[2]);
     server = gethostbyname(argv[1]);
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
-    }
+    }*/
+    // server->h_name is server hostname and can be used in inet_pton
+    // server->h_addr is server address and can be set to serv_addr.sin_addr.s_addr
 
     // Initialize the socket
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
+	  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) { 
+	  	printf("Socket creation error\n");
+	  	return -1;
+	  }
+
+    // START SERV_ADDR STUFF (also related to above)
 
     // Initialize Server Information
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(portno);
-    bcopy((char *) server->h_addr, 
-         (char *) &serv_addr.sin_addr.s_addr,
-         server->h_length);
+	  memset(&serv_addr, '0', sizeof(serv_addr));
+	  serv_addr.sin_family = AF_INET;
+	  serv_addr.sin_port = htons(PORT);
+
+	  // Convert IPv4 and IPv6 addresses from text to binary form 
+    //
+    // Note: inet_pton supports ipv4 and ipv6, but doesn't support ipv4
+    // shorthand like inet_aton, which doesn't support ipv6.
+
+	  if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) { 
+	  	printf("Invalid address/ Address not supported\n");
+	  	return -1;
+	  }
+    
+    // END SERV_ADDR STUFF
 
     // Connect to sock
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
+	  if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) { 
+	  	printf("Connection Failed \n");
+	  	return -1;
+	  }
+    
+    // TODO: Create an input loop
 
-    // Prompt user message from stdin
-    printf("Please enter the message: ");
-    bzero(buffer, 256);
-    fgets(buffer, 255, stdin);
+	  write(sock, hello, strlen(hello));
+	  printf("Hello message sent\n");
+	  valread = read(sock , buffer, 1024);
 
-    //char buffer[256];
-    // Write to buffer
-    n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0)
-         error("ERROR writing to socket");
-    bzero(buffer, 256);
-    n = read(sockfd, buffer, 255);
-    if (n < 0)
-         error("ERROR reading from socket");
-    printf("%s\n", buffer);
-    return 0;
+    // I can get the value returned from eax for some cases
+    // This is not the case for odd return sizes
+    // It does not work on Windows (I think)
+    // It does not work on inline functions
+    int i;
+    asm("movl %%eax, %0" : "=r" (i));
+    printf("The value of eax is %d\n", i);
+    //printf("The number of bytes read is %d\n", valread);
+    //
+    // SetErrorTestType(INT_SENTINEL)
+    // SetErrorTestType(ERRNO)
+    //    might be able to set to 0 after handling
+
+    // valread = bytes read, errno is set to != 0
+    // for safety, check valread to know if errno was updated instead of
+    // trying to say errno = 0 before each function that _may_ update the errno
+    // I can simply use perror, or use strerror(errno) in a formatted string
+	  printf("%s\n", buffer);
+	  return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,53 +1,65 @@
-/* A simple server in the internet domain using TCP
-   The port number is passed as an argument */
+// Server side C/C++ program to demonstrate Socket programming 
 
-#include <stdio.h>  // print
-#include <stdlib.h> // exit, atoi
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
 
-#include <sys/types.h> // ?
-#include <sys/socket.h>
-#include <netinet/in.h>
+#define PORT 1042
 
-#include <string.h> // bzero
-#include <unistd.h> // read, write
-
-void error(char *msg)
-{
-    perror(msg);
-    exit(1);
-}
-
-int main(int argc, char *argv[])
-{
-     int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, (unsigned int*) &clilen);
-     if (newsockfd < 0) 
-          error("ERROR on accept");
-     bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
-     if (n < 0) error("ERROR writing to socket");
-     return 0; 
+int main(int argc, char const *argv[])
+{ 
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+       
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    { 
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    // This returns an error on mac, but not linux. It is non-fatal,
+    // and if I comment it out nothing seems to happen.
+    // 
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt)))
+    { 
+        //perror("setsockopt");
+        //exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0)
+    { 
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    { 
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0)
+    { 
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read( new_socket , buffer, 1024);
+    printf("%s\n",buffer );
+    send(new_socket , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    return 0;
 }
